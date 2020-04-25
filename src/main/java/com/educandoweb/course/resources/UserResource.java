dcade687814d8c41430cb3b26ceca88490fc27fe
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-25 07:18:08 -0300

    Service layer, componente de registro

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserServices servico;
+	
 	@GetMapping
-	public ResponseEntity<User> buscaTodos(){
-		User u = new User(1L,"Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> buscaTodos(){
+		List<User> list = servico.buscaTodos();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> buscaPorId(@PathVariable Long id){
+		User obj = servico.buscaPorId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 	
